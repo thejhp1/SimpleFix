220c2e7ff5102d5b70c53fddad25988836c14bd5
thejhp1 <junhyung96@gmail.com>
2023-08-07 22:33:24 -0400

    backend

---
+++ backend/routes/api/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+// Add a XSRF-TOKEN cookie
+router.get("/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
+});
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+
+module.exports = router;
