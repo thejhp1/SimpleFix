6f820d9417f0d0a557ebb49b21a1f5fb1fe0edef
thejhp1 <junhyung96@gmail.com>
2023-08-08 16:47:45 -0400

    seeds

---
+++ backend/db/seeders/20230608195812-seed-demo-user.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const bcrypt = require("bcryptjs");
+
+let options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'Employees';
+    return queryInterface.bulkInsert(options, [
+      {
+        companyId: 1,
+        email: 'demo1@user.io',
+        username: 'Demo-A',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        companyId: 2,
+        email: 'demo2@user.io',
+        username: 'Demo-B',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Employees';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-A', 'Demo-B'] }
+    }, {});
+  }
+};
--- backend/db/seeders/20230808204459-seed-companies.js
+++ backend/db/seeders/20230808204459-seed-companies.js
@@ -1,7 +1,9 @@
 'use strict';
+
 const bcrypt = require("bcryptjs");
 
 let options = {};
+
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
@@ -9,20 +11,14 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    options.tableName = 'Employees';
+    options.tableName = 'Companies';
     return queryInterface.bulkInsert(options, [
       {
-        companyId: 1,
-        email: 'demo1@user.io',
-        username: 'Demo-A',
-        hashedPassword: bcrypt.hashSync('password')
+        name: "Samsung"
       },
       {
-        companyId: 2,
-        email: 'demo2@user.io',
-        username: 'Demo-B',
-        hashedPassword: bcrypt.hashSync('password')
-      },
+        name: "GE"
+      }
     ], {});
   },
 
@@ -30,7 +26,7 @@ module.exports = {
     options.tableName = 'Employees';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      username: { [Op.in]: ['Demo-A', 'Demo-B'] }
+      name: { [Op.in]: ['Samsung', 'GE'] }
     }, {});
   }
 };
