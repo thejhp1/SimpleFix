6d177a65db6b825f8885205a56e253a03cd9805a
thejhp1 <junhyung96@gmail.com>
2023-08-08 22:23:16 -0400

    fix

--- backend/db/migrations/20230808160631-create-company.js
+++ backend/db/migrations/20230808160631-create-company.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Companies", {
+    await queryInterface.createTable("Companys", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -34,7 +34,7 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    options.tableName = "Companies";
+    options.tableName = "Companys";
     await queryInterface.dropTable(options);
   },
 };
