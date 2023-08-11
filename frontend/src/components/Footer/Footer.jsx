ab8e45eb6c3ab401c0f484d86cb5c0d944d4a3fa
thejhp1 <junhyung96@gmail.com>
2023-08-10 23:46:00 -0400

    added links to developer section

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 import "../../styles/components/Footer.css";
 
 export default function Footer() {
+  const history = useHistory()
 
   const sendToTop = () => {
     window.scrollTo({ top: 1, behavior: "smooth" });
@@ -13,7 +15,7 @@ export default function Footer() {
         <div className='footer_inner'>
             <img onClick={sendToTop} src='/images/LandingPage_WhiteHorizontalLogo.png' />
             <p className='footer-developer'>
-                Meet the Developer: <i class="fa-brands fa-square-github fa-2xl"></i> <i class="fa-brands fa-linkedin fa-2xl"></i>
+                Meet the Developer: <i onClick={()=> window.location.href = "https://github.com/thejhp1"} class="fa-brands fa-square-github fa-2xl"></i> <i onClick={()=> window.location.href = "https://www.linkedin.com/in/jun-park-3b23b7285/"}  class="fa-brands fa-linkedin fa-2xl"></i>
             </p>
             <span>
                 <i class="fa-regular fa-copyright"></i> SimpleFix. No Rights Reserved.
