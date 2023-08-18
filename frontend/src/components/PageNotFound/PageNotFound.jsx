cbd42566a8a773e1fc8b5ea02824d76c5a0d1015
thejhp1 <junhyung96@gmail.com>
2023-08-17 23:46:44 -0400

    added not found page

--- frontend/src/components/PageNotFound/PageNotFound.jsx
+++ frontend/src/components/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import "../../styles/components/PageNotFound.css"
 
 export default function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <section className='page-not-found-container'>
+      <div className='page-not-found_inner'>
+        <p>Did you mean to do that? Nothing found.</p>
+        <img width="800px" height="800px" src="/images/NotFoundPage.png"/>
+      </div>
+    </section>
   )
 }
